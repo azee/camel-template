c90e2373e6b3b3fdaa06bf473952f2e9d62fb794
azee <azee@yandex-team.ru>
2014-03-29 23:51:54 +0400

    Tests

--- template-core/src/test/java/com/mycompany/template/PollTest.java
+++ template-core/src/test/java/com/mycompany/template/PollTest.java
@@ -1,16 +1,30 @@
 package com.mycompany.template;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.mycompany.template.beans.Competitor;
+import com.mycompany.template.beans.Poll;
+import com.mycompany.template.utils.TestBeansGenerator;
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
 import org.apache.camel.test.spring.MockEndpoints;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
 
 
 /**
@@ -20,7 +34,14 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration(locations = {"classpath:META-INF/spring/camel-context.xml", "classpath:camel-test-beans.xml", "classpath:jms-test.xml"})
 @DirtiesContext
 @MockEndpoints("*")
+@SuppressWarnings("SpringJavaAutowiringInspection")
 public class PollTest {
+
+    @Autowired
+    TestBeansGenerator testBeansGenerator;
+
+    @Autowired
+    HazelcastInstance instance;
 
     @EndpointInject(uri = "mock:activemq:queue:poll.aggregate")
     protected MockEndpoint pollAggregate;
@@ -37,7 +58,37 @@ public class PollTest {
     @DirtiesContext
     @Test
     public void createPollTest() throws InterruptedException {
+        String pollId = "Poll1";
+        pollQueue.sendBody(testBeansGenerator.getPoll(pollId));
+        pollAggregate.setExpectedMessageCount(2);
+        pollAggregate.assertIsSatisfied();
 
+        IMap map = instance.getMap("pollRepo");
+        assertNotNull(map);
+
+        Object state = map.get(pollId);
+        assertNotNull(state);
+        assertTrue(state instanceof Exchange);
+
+        Object poll = ((Exchange) state).getIn().getBody();
+        assertNotNull(poll);
+        assertTrue(poll instanceof Poll);
+
+        List<Competitor> competitors = ((Poll) poll).getCompetitors();
+        assertNotNull(competitors);
+        Assert.assertThat(competitors.size(), is(3));
+
+        assertNotNull(competitors.get(0));
+        assertNotNull(competitors.get(1));
+        assertNotNull(competitors.get(2));
+    }
+
+    @DirtiesContext
+    @Test
+    public void ignoreInitialVoteTest() throws InterruptedException {
+        pollQueue.sendBody(testBeansGenerator.getVote("Poll1", "1"));
+        pollAggregate.setExpectedMessageCount(0);
+        pollAggregate.assertIsSatisfied();
     }
 
 }
--- template-core/src/test/java/com/mycompany/template/utils/TestBeansGenerator.java
+++ template-core/src/test/java/com/mycompany/template/utils/TestBeansGenerator.java
@@ -1,5 +1,8 @@
 package com.mycompany.template.utils;
 
+import com.mycompany.template.beans.Competitor;
+import com.mycompany.template.beans.Poll;
+import com.mycompany.template.beans.Vote;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,5 +10,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TestBeansGenerator {
+
+    public Poll getPoll(String pollId){
+        Poll poll = new Poll();
+        poll.setPollId(pollId);
+
+        poll.getCompetitors().add(getCompetitor("1"));
+        poll.getCompetitors().add(getCompetitor("2"));
+        poll.getCompetitors().add(getCompetitor("3"));
+
+        return poll;
+    }
+
+    public Competitor getCompetitor(String id){
+        Competitor competitor = new Competitor();
+        competitor.setId(id);
+        competitor.setName(id);
+        return competitor;
+    }
+
+    public Vote getVote(String pollId, String competitorId){
+        Vote vote = new Vote();
+        vote.setCompetitorId(competitorId);
+        vote.setPollId(pollId);
+        return vote;
+    }
 
 }
